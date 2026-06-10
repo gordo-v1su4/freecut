b191d36abd22041262e717c25d0d18f238734fa9
walterlow <walterlow88@gmail.com>
2026-06-10 20:07:35 +0800

    style: apply oxfmt to drifted files

--- src/features/media-library/deps/scene-browser-contract.ts
+++ src/features/media-library/deps/scene-browser-contract.ts
@@ -3,12 +3,8 @@
  * the info popover through this contract.
  */
 
-export {
-  useSceneBrowserStore,
-} from '@/features/scene-browser/stores/scene-browser-store'
-export {
-  invalidateMediaCaptionThumbnails,
-} from '@/features/scene-browser/utils/invalidate'
+export { useSceneBrowserStore } from '@/features/scene-browser/stores/scene-browser-store'
+export { invalidateMediaCaptionThumbnails } from '@/features/scene-browser/utils/invalidate'
 
 export const importSceneBrowserPanel = () =>
   import('@/features/scene-browser/components/scene-browser-panel')
